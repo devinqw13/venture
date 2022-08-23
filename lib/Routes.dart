06ae9e99b357b4343610754f64fc8986b0e774b7
Devin Williams <devinqwilliams13@gmail.com>
2022-08-22 20:30:14 -0400

    update

--- lib/Routes.dart
+++ lib/Routes.dart
@@ -1,19 +1,17 @@
 import 'package:venture/Controllers/Dashboard/DashboardBinding.dart';
 import 'package:venture/Screens/DashboardScreen/DashboardScreen.dart';
 import 'package:get/route_manager.dart';
+import 'package:venture/Screens/UploadContentScreen/UploadContentScreen.dart';
 
 class Routes {
   static const INITIAL = '/home';
 
   static final routes = [
-    // GetPage(
-    //   name: '/login', 
-    //   page: () => LoginPage(),
-    // ),
     GetPage(
-      name: '/home', 
+      name: '/home',
       page: () => DashboardScreen(),
       binding: DashboardBinding(),
-    )
+      arguments: <dynamic, dynamic>{}
+    ),
   ];
 }
