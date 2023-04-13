1c48e9b2d35cba7bb2fa7a035086b52eaf22cb31
Devin Williams <devinqwilliams13@gmail.com>
2023-04-12 20:42:47 -0400

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -17,7 +19,9 @@ import 'package:venture/Routes.dart';
 import 'package:venture/Globals.dart' as globals;
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   await Firebase.initializeApp();
   await GetStorage.init();
 
@@ -65,6 +69,7 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       future = Future.value(true);
     });
+    FlutterNativeSplash.remove();
   }
 
   saveUniqueIDToSecureStorage() async {
@@ -166,40 +171,55 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<bool>(
-      future: future,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return GetMaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: "Venture",
-            theme: Themes.lightTheme,
-            darkTheme: Themes.darkTheme,
-            themeMode: getThemeMode(themeController.theme),
-            navigatorKey: navigatorKey,
-            // initialRoute: '/',
-            navigatorObservers: [routeObserver],
-            getPages: Routes.routes,
-            initialRoute: getInitialRoute(),
-            // routes: <String, WidgetBuilder>{
-            //   '/': (context) => getFirstScreen()
-            // },
-          );
-        }
-        else {
-          return Container(
-            color: primaryBlue,
-            child: Center(
-              child: Container()
-              // child: Image.asset('assets/images/ATS_HW_Vertical_FullColor_02.png',
-              //   alignment: Alignment.center,
-              //   fit: BoxFit.fill,
-              // ),
-            ),
-          );
-        }
-      }
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Venture",
+      theme: Themes.lightTheme,
+      darkTheme: Themes.darkTheme,
+      themeMode: getThemeMode(themeController.theme),
+      navigatorKey: navigatorKey,
+      // initialRoute: '/',
+      navigatorObservers: [routeObserver],
+      getPages: Routes.routes,
+      initialRoute: getInitialRoute(),
+      // routes: <String, WidgetBuilder>{
+      //   '/': (context) => getFirstScreen()
+      // },
     );
+    // return FutureBuilder<bool>(
+    //   future: future,
+    //   builder: (context, snapshot) {
+    //     if (snapshot.connectionState == ConnectionState.done) {
+    //       return GetMaterialApp(
+    //         debugShowCheckedModeBanner: false,
+    //         title: "Venture",
+    //         theme: Themes.lightTheme,
+    //         darkTheme: Themes.darkTheme,
+    //         themeMode: getThemeMode(themeController.theme),
+    //         navigatorKey: navigatorKey,
+    //         // initialRoute: '/',
+    //         navigatorObservers: [routeObserver],
+    //         getPages: Routes.routes,
+    //         initialRoute: getInitialRoute(),
+    //         // routes: <String, WidgetBuilder>{
+    //         //   '/': (context) => getFirstScreen()
+    //         // },
+    //       );
+    //     }
+    //     else {
+    //       return Container(
+    //         color: primaryBlue,
+    //         child: Center(
+    //           child: Container()
+    //           // child: Image.asset('assets/images/ATS_HW_Vertical_FullColor_02.png',
+    //           //   alignment: Alignment.center,
+    //           //   fit: BoxFit.fill,
+    //           // ),
+    //         ),
+    //       );
+    //     }
+    //   }
+    // );
   }
 }
 
